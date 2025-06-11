5ca95b74384b14ce71aafe14e0abd9fbc88160ed
AceleradorDeEmpresas <adceleradordeempresas@gmail.com>
2025-06-10 23:41:32 -0600

    Funciones existentes de PHP

---
+++ existing_functions.php
@@ -0,0 +1,25 @@
+<?php 
+
+    // Funciones existentes en PHP 
+    print_r(strtoupper("alex")); // Función que convierte una cadena a mayúsculas
+    print_r("\n");
+    print_r(strtolower("ALEJANDRO")); // Función que convierte una cadea a miúsculas
+    print_r("\n");
+    print_r(strlen("alex")); // Función que devuelve la longituo de una cadena
+    print_r("\n");
+    print_r(strpos("alex", "x")); // Función que devuelve la posición de la primera aparción de un caracter en una cadena
+    print_r("\n");
+    print_r(mb_strtoupper("José")); // FUnción que convierte una cadena a mayúsculas con acentos
+    print_r("\n");
+    print_r(mb_strlen("Magdiel")); // FUnción que devuelve la longitud de una cadena con acentos
+    print_r("\n");
+    
+    
+    // Funciones númericas
+    print_r(rand(1, 100)); // Función que gener un número aleatorio entre 1 y 100
+    print_r("\n");
+    
+    // Funciones de fecha y hora
+    print_r(date("Y-m-d H:i:s")); // Función que devuelve la fecha y hora actual
+
+    
