361123b6e61f79da1147fd28c27e5b4c2fa104a6
AceleradorDeEmpresas <adceleradordeempresas@gmail.com>
2025-07-06 22:44:18 -0600

    Union types

--- OOP/07-class.php
+++ OOP/07-class.php
@@ -1,3 +1,17 @@
 <?php
+    // Tipado extricto 
+    declare(strict_types=1);
+
 
     // Union types
+    class Animal {
+        public string $nombre;
+        public int $edad;
+        public string $servicio;
+        // Union type para el precio se usa | para declarar una unión de tipos
+        public int|float $precio;
+
+        function __construct() {
+
+        }
+    }
